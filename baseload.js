b8dc86866c181a0de3210b71e9ed514fbc3732ba
Gdiael Barros <gdiael.barros@gmail.com>
2023-08-06 16:13:07 -0300

    versão final

--- baseload.js
+++ baseload.js
@@ -1,7 +1,12 @@
+const appUrl = 'https://script.google.com/macros/s/AKfycbwm6FbHxj-8uSZ9ATEIzC6iDPz0sc2uZbRSJBW0B2_x_o-ARbVqWVNg_t_4BboZRg6L/exec'
+
+var selectedNumbers = [];
+
 $(document).ready(function() {
     $("#telefone").inputmask("(99) 99999-9999");
     $('#loader').hide();
     $('#rifa').hide();
+    $('#tabela').hide();
     $('#numrifa').prop("disabled", true);
     getNumbersInfo();
 });
@@ -18,8 +23,7 @@ function startLoad() {
 
 function btZapClick() {
     var numeroTelefone = "5584988054083";
-    var mensagem = "Quero comprar uma rifa para ajudar Seu Miguel Rogério."; 
-    var mensagemCodificada = encodeURIComponent(mensagem);
+    var mensagemCodificada = encodeURIComponent(whatsappMsg());
 
     var linkWhatsApp = "https://api.whatsapp.com/send?phone=" + numeroTelefone + "&text=" + mensagemCodificada;
 
@@ -27,33 +31,53 @@ function btZapClick() {
     .location.href = linkWhatsApp;
 }
 
+function whatsappMsg() {
+    let txt = 'Rifa seu Miguel Rogério.\n';
+    txt += $('#rifaCompra').text() + '\n';
+    txt +=  $('#rifasQuanti').text() + '\n';
+    txt +=  $('#rifasNumeros').text() + '\n';
+    txt +=  $('#rifasComprov').text();
+    return txt;
+}
+
+function btPixClick() {
+    navigator.clipboard.writeText('07100849438')
+    .then(() => {
+        showPopUp('Pix copiado!')
+    })
+    .catch(err => {
+        console.error("Erro ao copiar o texto:", err);
+    });
+}
+
 function enviarFormulario() {
     var nome = $("#nome").val();
     var telefone = $("#telefone").val();
-    var numRifa = $("#numrifa").val();
 
     if(nome == '') {
         showPopUp('Escreva o seu nome!');
         return;
     }
 
+    nome = nome.toUpperCase();
+
     if(telefone.replace('-','').replace('(','').replace(')','').replace('_','').replace(' ','').length != 11) {
         showPopUp('Número de telefone incorreto!');
         return;
     }
 
-    if(numRifa == '') {
-        showPopUp('Escolha um número de rifa!');
+    if(selectedNumbers.length < 1) {
+        showPopUp('Escolha pelo menos um número de rifa!');
         return;
     }
 
-    var dados = {nome: nome, telefone: telefone, numero: numRifa};
+    var dados = [];
 
-    url = 'https://script.google.com/macros/s/AKfycbxXxPc_sh6yTaZlBCAPhhwPro93f6f1hGnDXsZ9GlG4_2UMcIXym_XQvSztoXXQx6jn/exec'
+    selectedNumbers.forEach(obj => dados.push({nome: nome, telefone: telefone, numero: obj}));
 
     startLoad();
 
-    fetch(url, {
+    fetch(appUrl, {
         method: "POST",
         redirect: "follow",
         headers: {
@@ -63,12 +87,14 @@ function enviarFormulario() {
     })
     .then(response => response.json())
     .then(data => {
-        var divResposta = document.getElementById("rifa");
-        var text = divResposta.innerHTML
-        text = text.replace("[MMMMM]", nome);
-        text = text.replace("[TTTTT]", telefone);
-        text = text.replace("[CCCCC]", numero);
-        divResposta.innerHTML = text.replace("[XXXXX]", data.code);
+        replaceKey('rifaCompra', '[MMMMM]', nome);
+        replaceKey('rifaCompra', '[TTTTT]', telefone);
+        replaceKey('rifasQuanti', '[QQQQQ]', selectedNumbers.length);
+        let rifaNumbersTxt = ((selectedNumbers.length > 1) ? 'Rifas Nº: ' : 'Rifa Nº: ') + selectedNumbers.join(', ') + '.';
+        replaceKey('rifasNumeros', '[CCCCC]', rifaNumbersTxt);
+        let rifaCodes = ((selectedNumbers.length > 1) ? 'Códigos: ' : 'Código: ') +  data.codes.join(', ') + '.';
+        replaceKey('rifasComprov', '[XXXXX]', rifaCodes);
+
         stopLoad();
     })
     .catch(error => {
@@ -77,8 +103,13 @@ function enviarFormulario() {
     });
 }
 
+function replaceKey(elementId, key, txt) {
+    let elem = $('#' + elementId);
+    let newTxt = elem.text().replace(key, txt);
+    elem.text(newTxt);
+}
+
 function getNumbersInfo() {
-    const appUrl = 'https://script.google.com/macros/s/AKfycbxXxPc_sh6yTaZlBCAPhhwPro93f6f1hGnDXsZ9GlG4_2UMcIXym_XQvSztoXXQx6jn/exec'
     fetch(appUrl)
         .then(response => response.json())
         .then(data => {
@@ -91,6 +122,7 @@ function getNumbersInfo() {
 }
 
 function updateTable(jsonData) {
+    $('#loaderTbl').hide();
     tabela = document.getElementById("tabela");
     for (var i = 1; i < 501; i++) {
         var status = 'free';
@@ -101,6 +133,7 @@ function updateTable(jsonData) {
         });
         tabela.appendChild(buttonFactory(i, status));
     }
+    $('#tabela').show();
 }
 
 function buttonFactory(number, status) {
@@ -111,7 +144,17 @@ function buttonFactory(number, status) {
     if(status == 'free') {
         newButon.onclick = function() {
             let numero = $(this).text();
-            $('#numrifa').val(numero);
+            let ind = selectedNumbers.indexOf(numero);
+            if(ind == -1) {
+                if(selectedNumbers.length < 10) {
+                    selectedNumbers.push(numero);
+                    newButon.className = 'btnSel';    
+                }
+            } else {
+                selectedNumbers.splice(ind, 1);
+                newButon.className = 'free';
+            }
+            $('#numrifa').text(selectedNumbers.join(', ') + '.');
         };
     }
     return newButon;
@@ -122,5 +165,5 @@ function showPopUp(msg) {
     $("#popUp").fadeIn();
     setTimeout(function() {
       $("#popUp").fadeOut();
-    }, 1000);
+    }, 1500);
 }
